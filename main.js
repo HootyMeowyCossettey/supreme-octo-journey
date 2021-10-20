ff3bb74cb773b5fbe4ffa48b618f482173c74bb3
HootyMeowyCossettey <91523274+HootyMeowyCossettey@users.noreply.github.com>
2021-10-20 23:10:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var canvas=new fabric.Canvas("myCanvas");
+ var song=document.getElementById("myAudio");
+ var img="Happy Birthday (2).gif";
+ 
+
+function new_image()
+{
+	fabric.Image("BirthdayImage.jpg",function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:0
+            
+        });
+        canvas.add(block_image_object);
+        playSound();
+        
+    })
+	
+}
+
+function playSound(){
+	song.play(myAudio);
+    
+}
+
